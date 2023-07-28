0d126b768272384b0f23d3d0839cfe5a1a95e014
Shivam Ranabhat <shivamranabhat@gmail.com>
2023-07-28 23:09:51 +0545

    Slide menu added

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,10 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var bar = document.querySelector('.bar');
-    var svg = bar.querySelector('svg');
-    var slideDown = document.querySelector('.slide-down');
-
-    svg.addEventListener('click', function () {
-      bar.classList.toggle('active');
-      slideDown.classList.toggle('active');
-    });
+$(document).ready(function () {
+  $('.bar svg').on('click', function () {
+    $('.slide-down').toggleClass('active');
   });
+
+  $('.close svg').on('click', function () {
+    $('.slide-down').removeClass('active');
+  });
+});
